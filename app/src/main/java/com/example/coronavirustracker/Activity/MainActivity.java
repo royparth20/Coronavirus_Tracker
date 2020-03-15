2053d3d175598d01803ae4ab19db6eb6f0aab48c
royparth20 <royparth2094@gmail.com>
2020-03-15 11:20:46 +0530

    15-03-20 11:23AM Exit Dailog

--- app/src/main/java/com/example/coronavirustracker/Activity/MainActivity.java
+++ app/src/main/java/com/example/coronavirustracker/Activity/MainActivity.java
@@ -1,32 +1,29 @@
 package com.example.coronavirustracker.Activity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.example.coronavirustracker.Adapter.BaseAdapter;
 import com.example.coronavirustracker.Fragment.NavigationDrawerFragment;
-import com.example.coronavirustracker.Modal.Data;
 import com.example.coronavirustracker.R;
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "com.example.coronavirustracker.Activity.MainActivity";
-    private List<Data> dataList;
-    private BaseAdapter adapter;
     private BottomAppBar bar;
 
     @SuppressLint({"WrongConstant", "ResourceAsColor"})
@@ -35,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        NavigationView navigationView = findViewById(R.id.navigation);
         bar = findViewById(R.id.bar);
         bar.setTitle(R.string.app_name);
         bar.setTitleTextColor(R.color.colorAccent);
@@ -84,33 +80,45 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
+
+//        new android.app.AlertDialog.Builder(this).setTitle("Exit App").setMessage("Do you want to exit app?").setPositiveButton("Yes", new ExitApp()).setNegativeButton("No", null).show();
         show_Exit();
     }
 
+    private class ExitApp implements DialogInterface.OnClickListener {
+        @SuppressLint("NewApi")
+        @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            finishAffinity();
+            System.exit(0);
+        }
+    }
     private void show_Exit() {
         String positiveText = getString(android.R.string.ok);
         String negativeText = getString(android.R.string.cancel);
-        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(getApplicationContext())
+        new MaterialAlertDialogBuilder(this, R.style.CutShapeTheme)
                 .setTitle(getString(R.string.label_exit_app))
                 .setMessage(getString(R.string.description_sure_exit_app)).setPositiveButton(positiveText,
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // dismiss alert dialog, update preferences with game score and restart play fragment
-                                Log.d("myTag", "positive button clicked");
-                                dialog.dismiss();
-                            }
-                        }).setNegativeButton(negativeText,
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // dismiss dialog, start counter again
-                                dialog.dismiss();
-                                Log.d("myTag", "negative button clicked");
-                            }
-                        });
-
-        builder.show();
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // dismiss alert dialog, update preferences with game score and restart play fragment
+                        Log.d("myTag", "positive button clicked");
+                        dialog.dismiss();
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                            finishAffinity();
+                        }
+                        System.exit(0);
+                    }
+                }).setNegativeButton(negativeText,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // dismiss dialog, start counter again
+                        dialog.dismiss();
+                        Log.d("myTag", "negative button clicked");
+                    }
+                }).create().show();
     }
 }
